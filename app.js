17883046e9b362bb77dac243b99a81dee1311aa3
AndyChienn <andy2002chien@hotmail.com.tw>
2022-05-11 11:28:22 +0800

    使用JSON檔案設置index及show頁面

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render("index")
+  res.render("index", { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id.toString())
+  res.render("show", { restaurant: restaurant })
 })
 
 app.listen(port, () => {
