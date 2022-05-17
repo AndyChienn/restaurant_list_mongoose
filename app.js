c9a6f06175c9b8515890be491931dae991a70dec
AndyChienn <andy2002chien@hotmail.com.tw>
2022-05-17 13:05:34 +0800

    feat: add Mongoose

--- app.js
+++ app.js
@@ -3,11 +3,27 @@ const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+const mongoose = require('mongoose')
+const db = mongoose.connection
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.render("index", { restaurants: restaurantList.results })
